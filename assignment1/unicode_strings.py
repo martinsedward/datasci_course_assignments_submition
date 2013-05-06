212f3265a97074bc4a4dea9c3279ff69326b42ae
martinsedward <martins_edward@hotmail.com>
2013-05-05 21:50:39 -0300

    unicode_strings

---
+++ assignment1/unicode_strings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import urllib
+import json
+
+
+unicode_string = u"ç*&%$#áéúãõ"
+print unicode_string
+encoded_string = unicode_string.encode('utf-8')
+print encoded_string
+#print "Hello CrasyWorld"
+
