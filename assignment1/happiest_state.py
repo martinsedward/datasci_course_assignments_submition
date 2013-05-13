156d6f63d938b2f2cf161f6ae688cc6e6369bdae
OpenShift guest <edward.junior@gojames.com.br>
2013-05-12 21:08:37 -0400

    happiest_state

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -38,9 +38,9 @@ try:
                     score = score + scores.get(word, 0)
                     word_aux = word_aux + str(word) + " "
                     citie, value = json.loads(jsonstring)['place']['full_name'].split(",")
-                    state_aux = json.loads(jsonstring)['place']['country_code'] + " - " + citie + " - " + value #  + " - " + json.loads(jsonstring)['user']['location']
+                    #state_aux = json.loads(jsonstring)['place']['country_code'] + " - " + citie + " - " + value #  + " - " + json.loads(jsonstring)['user']['location']
                 
-                print str(score),state_aux, " - [" + word_aux + "]"
+                #print str(score),state_aux, " - [" + word_aux + "]"
                 #Calculate whats the more happy state
                 for key_uf, value_uf in UF.iteritems():
                     score_uf, uf  = value_uf.split("\t")
@@ -66,9 +66,9 @@ try:
         if int(score) > score_aux:
             score_aux = int(score)
             Happy_UF = uf
-        print "%s: %s" % (score, uf)
+        #print "%s: %s" % (score, uf)
         
-    print "Happiest UF is "+Happy_UF + " with " + str(score_aux)
-    #print Happy_UF
+    #print "Happiest UF is "+Happy_UF + " with " + str(score_aux)
+    print Happy_UF
 except: 
     pass    
