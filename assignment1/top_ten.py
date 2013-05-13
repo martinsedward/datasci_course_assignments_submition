678632363bf4ae0e1d49942a901b28b909eb900f
OpenShift guest <edward.junior@gojames.com.br>
2013-05-13 00:41:36 -0400

    top_ten

--- assignment1/top_ten.py
+++ assignment1/top_ten.py
@@ -17,11 +17,11 @@ hashtags_text_list = {}
 index = 0
 
 for tweet in tweets:
-  if tweet['entities']['hashtags']:
-      for hashtags in tweet['entities']['hashtags']:
-        hashtags_text_list[index] = hashtags['text'].lower()
-        index = index + 1
-        break
+    if tweet['entities']['hashtags']:
+        for hashtags in tweet['entities']['hashtags']:
+            hashtags_text_list[index] = hashtags['text'].lower()
+            index = index + 1
+            break
 
 hashtags_text = {}
 for key, value in hashtags_text_list.iteritems():
@@ -36,4 +36,5 @@ for key, value in sorted(hashtags_text.iteritems(), key=lambda (k,v): (v,k), rev
     if count < 10:
         print key, value
         count = count + 1
-    
+    else:
+        break
