b096a199b6739bda8d86ea6468bb3ec9118cb6c4
OpenShift guest <edward.junior@gojames.com.br>
2013-05-12 21:14:06 -0400

    happiest_state

--- assignment1/happiest_state.py
+++ assignment1/happiest_state.py
@@ -37,10 +37,12 @@ try:
                 for word in pattern_split.split(json.loads(jsonstring)["text"].lower()):#Words of line
                     score = score + scores.get(word, 0)
                     word_aux = word_aux + str(word) + " "
-                    citie, value = json.loads(jsonstring)['place']['full_name'].split(",")
-                    #state_aux = json.loads(jsonstring)['place']['country_code'] + " - " + citie + " - " + value #  + " - " + json.loads(jsonstring)['user']['location']
                 
-                #print str(score),state_aux, " - [" + word_aux + "]"
+                citie, value = json.loads(jsonstring)['place']['full_name'].split(",")
+                UF[value] = str(score) + "\t" +value
+                state_aux = json.loads(jsonstring)['place']['country_code'] + " - " + citie + " - " + value #  + " - " + json.loads(jsonstring)['user']['location']
+                
+                print str(score),state_aux, " - [" + word_aux + "]"
                 #Calculate whats the more happy state
                 for key_uf, value_uf in UF.iteritems():
                     score_uf, uf  = value_uf.split("\t")
@@ -66,9 +68,9 @@ try:
         if int(score) > score_aux:
             score_aux = int(score)
             Happy_UF = uf
-        #print "%s: %s" % (score, uf)
+        print "%s: %s" % (score, uf)
         
-    #print "Happiest UF is "+Happy_UF + " with " + str(score_aux)
-    print Happy_UF
+    print "Happiest UF is "+Happy_UF + " with " + str(score_aux)
+    #print Happy_UF
 except: 
     pass    
