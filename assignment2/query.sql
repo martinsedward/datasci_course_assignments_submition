08a043a36e71e6006a3145bf30c592e01162d9de
OpenShift guest <edward.junior@gojames.com.br>
2013-05-16 00:57:54 -0400

    Queries

--- assignment2/query.sql
+++ assignment2/query.sql
@@ -1,15 +1,16 @@
 /* frequency(docid, term, count) */
 -- select count(*) from frequency where docid = "10398_txt_earn"
 -- select count(term) from frequency where docid = "10398_txt_earn" and count = 1;
+-- select count(*)
+-- from(
+--   select term
+--   from frequency 
+--   where docid = "10398_txt_earn" and count = 1
+--   union
+--   select term
+--   from frequency 
+--  where docid = "925_txt_trade" and count = 1
+-- ) x
+-- ;
 
-select count(*)
-from(
-  select term
-  from frequency 
-  where docid = "10398_txt_earn" and count = 1
-  union
-  select term
-  from frequency 
- where docid = "925_txt_trade" and count = 1
-) x
-;
+select count(*) from frequency where term = "parliament";
