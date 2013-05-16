a3171e03054ac6cf02802c5b292f89d3f1716299
OpenShift guest <edward.junior@gojames.com.br>
2013-05-16 00:11:50 -0400

    Queries

--- assignment2/query.sql
+++ assignment2/query.sql
@@ -1,3 +1,4 @@
 /* frequency(docid, term, count) */
+-- select count(*) from frequency where docid = "10398_txt_earn"
 
 select count(term) from frequency where docid = "10398_txt_earn" and count = 1;
