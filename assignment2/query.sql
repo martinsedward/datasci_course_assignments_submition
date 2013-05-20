4c690ea6de953162e9e63bae26612f0ad6ffa998
OpenShift guest <edward.junior@gojames.com.br>
2013-05-20 15:28:57 -0400

    Queries

--- assignment2/query.sql
+++ assignment2/query.sql
@@ -13,7 +13,7 @@
 -- ) x
 -- ;
 -- select count(*) from frequency where term = "parliament";
-select sum(Count_term) from (select count(term) AS 'Count_term' from frequency group by docid having count(term) > 300) as a;
+-- select sum(Count_term) from (select count(term) AS 'Count_term' from frequency group by docid having count(term) > 300) as a;
+-- You've probably got the group by and the filter correct, but remember that you need to add up the term frequencies rather than just count the terms.
 
-
---You've probably got the group by and the filter correct, but remember that you need to add up the term frequencies rather than just count the terms.
+select docid, term, count(term) from frequency group by docid having count(term) > 300;
